371598edb1da0d936b831cec160835b37b10134e
Marc Ruiz <marc.ruiz@gmail.com>
2009-01-10 17:01:09 +0000

    Actualització de les preferències predeterminades.

--- defaults/preferences/foxreplace.js
+++ defaults/preferences/foxreplace.js
@@ -1,4 +1,4 @@
 pref("extensions.fox@replace.fx.description", "chrome://foxreplace/locale/foxreplace.properties");
-pref("extensions.foxreplace.substitutionListXml", '<substitutionlist version="0.8"/>');
+pref("extensions.foxreplace.substitutionListXml", '<substitutionlist version="0.10"/>');
 pref("extensions.foxreplace.autoReplaceOnLoad", false);
 pref("extensions.foxreplace.replaceUrls", false);
