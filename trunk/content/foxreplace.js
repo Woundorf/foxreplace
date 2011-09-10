c1640db73cf63792ad5c6b4764458bd1f6939b4b
marc.ruiz@gmail.com <marc.ruiz@gmail.com@d68392fb-a253-0410-9073-1d998d35ae54>
2011-09-10 23:02:12 +0000

    L'opció d'autosubstitució s'actualitza d'acord amb la preferència al menú d'aplicació.

--- trunk/content/foxreplace.js
+++ trunk/content/foxreplace.js
@@ -5,7 +5,7 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
@@ -32,7 +32,7 @@
  * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 
 /**
@@ -40,7 +40,7 @@
  * manages the main UI.
  */
 var foxreplace = {
-  
+
   /**
    * Initialization code.
    */
@@ -49,48 +49,48 @@ var foxreplace = {
     //        .addEventListener("popupshowing",
     //                          function() { foxreplace.onShowContextMenu(); },
     //                          false);
-    
+
     this.prefs.service.addObserver("", this, false);
-    
+
     gBrowser.addEventListener("DOMContentLoaded", this.onPageLoad, true);
-    
+
     this._substitutionList = this.prefs.substitutionListXml;
     this.setAutoReplaceOnLoad(this.prefs.autoReplaceOnLoad);
-    
+
     // subscription
     var enableSubscription = this.prefs.enableSubscription;
     var subscriptionUrl = this.prefs.subscriptionUrl;
     var subscriptionPeriod = this.prefs.subscriptionPeriod;
-    
+
     if (enableSubscription && subscriptionUrl && subscriptionPeriod > 0)
       fxrSubscription.start(subscriptionUrl, subscriptionPeriod);
   },
-  
+
   /**
    * Finalization code.
    */
   onUnload: function() {
     this.prefs.service.removeObserver("", this);
   },
-  
+
   /**
    * Observes changes in preferences.
    */
   observe: function(aSubject, aTopic, aData) {
     // aSubject is the nsIPrefBranch we're observing (after appropriate QI)
     // aData is the name of the pref that's been changed (relative to aSubject)
-    
+
     if (aTopic != "nsPref:changed") return;
-    
+
     switch (aData) {
       case "substitutionListXml":
         this._substitutionList = this.prefs.substitutionListXml;
         break;
-      
+
       case "autoReplaceOnLoad":
         this.setAutoReplaceOnLoad(this.prefs.autoReplaceOnLoad);
         break;
-        
+
       case "enableSubscription":
       case "subscriptionUrl":
       case "subscriptionPeriod":
@@ -104,18 +104,19 @@ var foxreplace = {
         break;
     }
   },
-  
+
   /**
    * Sets auto-replace on load setting.
    */
   setAutoReplaceOnLoad: function(aAutoReplaceOnLoad) {
     document.getElementById("fxrMenuToolsFoxReplaceAutoReplaceOnLoad").setAttribute("checked", aAutoReplaceOnLoad);
+    document.getElementById("fxrAppMenuReplaceAutoReplaceOnLoad").setAttribute("checked", aAutoReplaceOnLoad);
     var menuItem = document.getElementById("fxrToolbarButtonMenuAutoReplaceOnLoad");
     if (menuItem) menuItem.setAttribute("checked", aAutoReplaceOnLoad);
-    
+
     this._autoReplaceOnLoad = aAutoReplaceOnLoad;
   },
-  
+
   /**
    * Shows the replace bar.
    */
@@ -125,7 +126,7 @@ var foxreplace = {
     // without the timeout it doesn't get the focus
     window.setTimeout("document.getElementById('fxrReplaceBarInputStringTextBox').focus();", 100);
   },
-  
+
   /**
    * Applies the substitution entered by the user in the replace bar.
    */
@@ -135,12 +136,12 @@ var foxreplace = {
     var outputString = document.getElementById("fxrReplaceBarOutputStringTextBox").value;
     var caseSensitive = document.getElementById("fxrReplaceBarCaseSensitiveCheckBox").checked;
     var html = document.getElementById("fxrReplaceBarHtmlCheckBox").checked;
-    
+
     if (inputString == "") return;  // this should not happen
-    
+
     // save substitution list
     var substitutionList = this._substitutionList;
-    
+
     try {
       // new temporal substitution list with only one item
       this._substitutionList =
@@ -153,32 +154,32 @@ var foxreplace = {
     catch (se) {  // SyntaxError
       this.prompts.alert(this.getLocalizedString("regExpError"), se);
     }
-    
+
     // restore substitution list
     this._substitutionList = substitutionList;
   },
-  
+
   /**
    * Applies substitutions from the substitution list.
    */
   listReplace: function() {
     this.replaceDocXpath();
   },
-  
+
   /**
    * Toggles auto-replace on load setting.
    */
   toggleAutoReplaceOnLoad: function() {
     this.prefs.autoReplaceOnLoad = !this.prefs.autoReplaceOnLoad;
   },
-  
+
   /**
    * Shows options dialog.
    */
   showOptions: function() {
     window.openDialog("chrome://foxreplace/content/options.xul", "", "chrome,titlebar,toolbar,centerscreen,modal");
   },
-  
+
   /**
    * Shows help file in a new tab.
    */
@@ -186,65 +187,65 @@ var foxreplace = {
     // Add tab, then make active
     gBrowser.selectedTab = gBrowser.addTab("chrome://foxreplace/content/help.xhtml");
   },
-  
+
   /**
    * Applies substitutions from the substitution list to the loaded page if
    * auto-replace on load is on.
    */
   onPageLoad: function(aEvent) {
     if (!foxreplace._autoReplaceOnLoad) return;
-    
+
     // doc is the document that triggered "onload" event
     var doc = aEvent.originalTarget;
     if (doc.nodeName != "#document") return;
-    
+
     // perform substitutions on the loaded document
     foxreplace.replaceDocXpath(doc.defaultView);
   },
-  
+
   /**
    * Performs susbstitutions from the substitution list in the passed window. If
    * no window is passed the current window is the target.
    */
   replaceDocXpath: function(aWindow) {
     if (!aWindow) aWindow = window.content;
-    
+
     if (aWindow.frames.length > 0) {
       var nFrames = aWindow.frames.length;
       for (var i = 0; i < nFrames; i++) this.replaceDocXpath(aWindow.frames[i]);
     }
-    
+
     var doc = aWindow.document;
     if (!doc || !("body" in doc)) return;
-    
+
     var url = doc.URL;
     if (!url) return;
     var nSubstitutions = this._substitutionList.length;
-    
+
     for (var j = 0; j < nSubstitutions; j++) {
       var group = this._substitutionList[j];
-      
+
       if (!group.matches(url)) continue;
-      
+
       if (group.html) this.replaceHtml(doc, group);
       else this.replaceText(doc, group);
     }
   },
-  
+
   /**
    * Applies substitutions from aGroup to aDocument on text.
    */
   replaceText: function(aDocument, aGroup) {
     // Replace title
     aDocument.title = aGroup.replace(aDocument.title);
-    
+
     // selection string possibilities
     /* //text()[name(parent::*)!='script']  :( name can be upper or lower */
     /* ... empty(index-of(('style'),lower-case(name(parent::*))))  :( functions not supported */
     /* //body//text()[string-length(normalize-space())>0] */
-    
+
     // TODO any other xml document won't be replaced
-    
+
     // Replace text nodes
     var textNodesXpath = "/html/head/title/text()"
                        + "|/html/body/text()"
@@ -304,7 +305,7 @@ var foxreplace = {
       var textNode = textNodes.snapshotItem(i);
       textNode.textContent = aGroup.replace(textNode.textContent);
     }
-    
+
     // Replace nodes with a "value" property
     var valueNodesXpath = "/html/body//input[@type='text']"
                         + "|/html/body//textarea"
@@ -330,7 +331,7 @@ var foxreplace = {
       valueNode.value = aGroup.replace(valueNode.value);
     }
   },
-  
+
   /**
    * Applies substitutions from aGroup to aDocument on HTML.
    */
@@ -338,7 +339,7 @@ var foxreplace = {
     var body = aDocument.evaluate("/html/body", aDocument, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
     body.singleNodeValue.innerHTML = aGroup.replace(body.singleNodeValue.innerHTML);
   }
-  
+
 };
 
 Components.utils.import("resource://foxreplace/subscription.js");
