4d2d43bdeeb0d18d87a227280b105b9507368ee5
marc.ruiz <marc.ruiz@gmail.com>
2012-08-18 18:22:57 +0000

    S'afegeix un nom als grups de substitució, es canvia el format de la llista a 0.14 i es marca la propera versió com a 0.14.0.

--- defaults/preferences/foxreplace.js
+++ defaults/preferences/foxreplace.js
@@ -1,5 +1,5 @@
 pref("extensions.fox@replace.fx.description", "chrome://foxreplace/locale/foxreplace.properties");
-pref("extensions.foxreplace.substitutionListJSON", '{"version":"0.13","groups":[]}');
+pref("extensions.foxreplace.substitutionListJSON", '{"version":"0.14","groups":[]}');
 pref("extensions.foxreplace.autoReplaceOnLoad", false);
 pref("extensions.foxreplace.replaceUrls", false);
 pref("extensions.foxreplace.enableSubscription", false);
