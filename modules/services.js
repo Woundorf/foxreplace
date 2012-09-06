c99d2646d30d722ff94c2f009954e1a28108bc52
marc.ruiz <marc.ruiz@gmail.com>
2012-09-06 18:46:30 +0000

    S'actualitza la llargada de línia estàndard del codi a 160 caràcters.

--- modules/services.js
+++ modules/services.js
@@ -1,37 +1,26 @@
 /* ***** BEGIN LICENSE BLOCK *****
  *   Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
+ * The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+ * specific language governing rights and limitations under the License.
  *
  * The Original Code is FoxReplace.
  *
- * The Initial Developer of the Original Code is
- * Marc Ruiz Altisent.
- * Portions created by the Initial Developer are Copyright (C) 2009
- * the Initial Developer. All Rights Reserved.
+ * The Initial Developer of the Original Code is Marc Ruiz Altisent.
+ * Portions created by the Initial Developer are Copyright (C) 2009-2012 the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
  *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- * 
+ * Alternatively, the contents of this file may be used under the terms of either the GNU General Public License Version 2 or later (the "GPL"), or the GNU
+ * Lesser General Public License Version 2.1 or later (the "LGPL"), in which case the provisions of the GPL or the LGPL are applicable instead of those above.
+ * If you wish to allow use of your version of this file only under the terms of either the GPL or the LGPL, and not to allow others to use your version of this
+ * file under the terms of the MPL, indicate your decision by deleting the provisions above and replace them with the notice and other provisions required by
+ * the GPL or the LGPL. If you do not delete the provisions above, a recipient may use your version of this file under the terms of any one of the MPL, the GPL
+ * or the LGPL.
+ *
  * ***** END LICENSE BLOCK ***** */
 
 /**
@@ -41,36 +30,34 @@
 var EXPORTED_SYMBOLS = ["prompts", "getLocalizedString"];
 
 var prompts = {
-  
+
   /**
    * Returns the prompt service.
    */
   get service() {
     if (!this._service)
-      this._service = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                .getService(Components.interfaces.nsIPromptService);
-    
+      this._service = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
+
     return this._service;
   },
-  
+
   /**
    * Shows an alert.
    */
   alert: function(aTitle, aText) {
     this.service.alert(null, aTitle, aText);
   },
-  
+
   /**
    * Shows a prompt without checkbox.
    */
   prompt: function(aTitle, aText, aValue) {
     return this.service.prompt(null, aTitle, aText, aValue, null, {});
   }
-  
+
 };
 
-var stringBundle = Components.classes["@mozilla.org/intl/stringbundle;1"]
-                             .getService(Components.interfaces.nsIStringBundleService)  
+var stringBundle = Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService)
                              .createBundle("chrome://foxreplace/locale/foxreplace.properties");
 
 function getLocalizedString(aKey, aValues) {
