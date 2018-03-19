e11735d1d414809ee54836b20ff176f812400b72
Marc Ruiz <marc.ruiz@gmail.com>
2018-03-19 13:35:50 +0100

    Update config_build.sh.

--- config_build.sh
+++ config_build.sh
@@ -2,7 +2,7 @@
 # Build config for build.sh
 APP_NAME=foxreplace
 CLEAN_UP=1
-ROOT_FILES="manifest.json *.js"
+ROOT_FILES="manifest.json"
 ROOT_DIRS="_locales icons lib options popup scripts sidebar"
 BEFORE_BUILD=
 AFTER_BUILD=
