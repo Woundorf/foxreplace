3e118a06b46447821702a0d25e798231b8141e53
Marc Ruiz <marc.ruiz@gmail.com>
2019-11-17 17:57:30 +0100

    WIP indexeddb

--- scripts/storage.js
+++ scripts/storage.js
@@ -14,76 +14,137 @@
  *
  *  ***** END LICENSE BLOCK ***** */
 
-var storage = {
+var storage = (() => {
 
-  getList() {
-    return browser.storage.local.get("list").then(results => {
-      if (results.list) return substitutionListFromJSON(results.list);
-      else return [];
-    });
-  },
+  const DATABASE_NAME = 'list';
 
-  setList(list) {
-    return browser.storage.local.set({ list: substitutionListToJSON(list) });
-  },
+  let db = new Dexie(DATABASE_NAME);
+  db.version(1).stores({
+    groups: '++id, enabled, mode, index',
+    substitutions: '++id, groupId, index'
+  });
 
-  getPrefs() {
-    return browser.storage.local.get({
-      enableContextMenu: false,
-      autoReplaceOnLoad: false,
-      autoReplacePeriodically: false,
-      autoReplacePeriod: 1,
-      replaceUrls: false,
-      replaceScripts: false,
-      enableSubscription: false,
-      subscriptionUrl: "",
-      subscriptionPeriod: 1
-    });
-  },
+  let storage = {
 
-  setPrefs(prefs) {
-    let sanitizedPrefs = {};
-    for (let p in prefs) {
-      switch (p) {
-        case "enableContextMenu":
-        case "autoReplaceOnLoad":
-        case "autoReplacePeriodically":
-        case "replaceUrls":
-        case "replaceScripts":
-        case "enableSubscription":
-          sanitizedPrefs[p] = Boolean(prefs[p]);
-          break;
-        case "autoReplacePeriod":
-        case "subscriptionPeriod":
-          sanitizedPrefs[p] = Number(prefs[p]);
-          break;
-        case "subscriptionUrl":
-          sanitizedPrefs[p] = String(prefs[p]);
-          break;
-        default:
-          console.warn("Unrecognized preference: ", p, prefs[p]);
-          break;
+    /// Migrates the local storage list to indexed db if necessary.
+    migrateToIndexedDb() {
+      return browser.storage.local.get('list').then(data => {
+        if (data.list !== undefined) {
+          //browser.storage.local.remove('list');
+          return this.setList(data.list);
+        }
+      });
+    },
+
+    getList() {
+      //return browser.storage.local.get("list").then(results => {
+      //  if (results.list) return substitutionListFromJSON(results.list);
+      //  else return [];
+      //});
+      return db.groups.toArray();
+    },
+
+    async setList(list) {
+      //return browser.storage.local.set({ list: substitutionListToJSON(list) });
+      // list is json
+      await db.delete();
+      await db.open();
+      await db.transaction('rw', db.groups, db.substitutions, async () => {
+        for (let i = 0; i < list.groups.length; i++) {
+          let group = list.groups[i];
+          let groupId = await db.groups.add({
+            name: group.name,
+            urls: group.urls,
+            html: group.html,
+            enabled: Number(group.enabled),
+            mode: group.mode,
+            index: i
+          });
+
+          await db.substitutions.bulkAdd(group.substitutions.map((substitution, index) => {
+            substitution.groupId = groupId;
+            substitution.index = index;
+            return substitution;
+          }));
+        }
+      });
+    },
+
+    /// Returns the groups with some data of their first substitution (to show in the options page).
+    getGroupsPreview() {
+      return db.transaction('r', db.groups, db.substitutions, async () => {
+        let groups = await db.groups.orderBy('id').toArray();
+        let firstSubstitutions = await db.substitutions.where({ index: 0}).sortBy('groupId');
+        return groups.map((group, index) => {
+          group.enabled = Boolean(group.enabled);
+          group.input = firstSubstitutions[index].input;
+          group.output = firstSubstitutions[index].output;
+          return group;
+        }).sort((group1, group2) => group1.index - group2.index);
+      });
+    },
+
+    getPrefs() {
+      return browser.storage.local.get({
+        enableContextMenu: false,
+        autoReplaceOnLoad: false,
+        autoReplacePeriodically: false,
+        autoReplacePeriod: 1,
+        replaceUrls: false,
+        replaceScripts: false,
+        enableSubscription: false,
+        subscriptionUrl: "",
+        subscriptionPeriod: 1
+      });
+    },
+
+    setPrefs(prefs) {
+      let sanitizedPrefs = {};
+      for (let p in prefs) {
+        switch (p) {
+          case "enableContextMenu":
+          case "autoReplaceOnLoad":
+          case "autoReplacePeriodically":
+          case "replaceUrls":
+          case "replaceScripts":
+          case "enableSubscription":
+            sanitizedPrefs[p] = Boolean(prefs[p]);
+            break;
+          case "autoReplacePeriod":
+          case "subscriptionPeriod":
+            sanitizedPrefs[p] = Number(prefs[p]);
+            break;
+          case "subscriptionUrl":
+            sanitizedPrefs[p] = String(prefs[p]);
+            break;
+          default:
+            console.warn("Unrecognized preference: ", p, prefs[p]);
+            break;
+        }
       }
+      return browser.storage.local.set(sanitizedPrefs);
+    },
+
+    getAutomaticGroups() {
+      return this.getList().then(list => list.filter(group => group.enabled && (group.mode == group.MODE_AUTO_AND_MANUAL || group.mode == group.MODE_AUTO)));
+    },
+
+    getManualGroups() {
+      return this.getList().then(list => list.filter(group => group.enabled && (group.mode == group.MODE_AUTO_AND_MANUAL || group.mode == group.MODE_MANUAL)));
+    },
+
+    getMainColumnState() {
+      return browser.storage.local.get("mainColumnState").then(result => {
+        return result.mainColumnState;
+      });
+    },
+
+    setMainColumnState(columnState) {
+      return browser.storage.local.set({ mainColumnState: columnState });
     }
-    return browser.storage.local.set(sanitizedPrefs);
-  },
 
-  getAutomaticGroups() {
-    return this.getList().then(list => list.filter(group => group.enabled && (group.mode == group.MODE_AUTO_AND_MANUAL || group.mode == group.MODE_AUTO)));
-  },
+  };
 
-  getManualGroups() {
-    return this.getList().then(list => list.filter(group => group.enabled && (group.mode == group.MODE_AUTO_AND_MANUAL || group.mode == group.MODE_MANUAL)));
-  },
+  return storage;
 
-  getMainColumnState() {
-    return browser.storage.local.get("mainColumnState").then(result => {
-      return result.mainColumnState;
-    });
-  },
-
-  setMainColumnState(columnState) {
-    return browser.storage.local.set({ mainColumnState: columnState });
-  }
-
-};
+})();
